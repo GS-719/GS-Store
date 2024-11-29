d9a0e49f110775272c48862cb19f741baae238e4
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-11-29 17:21:57 +0530

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Logo from "@/img/Logo.png";
+import Image from 'next/image';
+import Link from 'next/link';
+import "@/styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <header className='MainNavbar'>
+        <div className='FirstSection'>
+          <Link href="/" className='FirstSectionLink'>
+            <Image className='LogoImg' src={Logo} alt="Logo" width={100} height={50} />
+            <p className='LogoText'>GS Store</p>
+          </Link>
+        </div>
+        <nav className='SenondSection'>
+          <Link className='SecondSectionLink' href="/">Home</Link>
+          <Link className='SecondSectionLink' href="/">About</Link>
+          <Link className='SecondSectionLink' href="/">Home</Link>
+          <Link className='SecondSectionLink' href="/">Home</Link>
+        </nav>
+        <div className='ThirdSection'>
+          <input className='thirdSectionSearch' placeholder='Search' />
+        </div>
+        <div className='FourthSection'>
+          <button className='Button'>Sign in</button>
+          <button className='Button'>Sign up</button>
+        </div>
+      </header>
+      <hr/>
+    </>
+  )
+}
+
+export default Navbar
+
