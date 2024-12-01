7a241ede623d6991478bd5fe1c347935ea2b2e98
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-12-01 11:12:20 +0530

    Update getallusers.ts

--- src/pages/api/getallusers.ts
+++ src/pages/api/getallusers.ts
@@ -1,24 +1,16 @@
-import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
-const prisma = new PrismaClient();
+import prisma from "@/lib/prisma";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "GET") {
-        const Data = await prisma.user.findMany();
-        const data = [
-            {
-                id: 1,
-                username: "GS",
-                email: "gurjotsingh.code@gmail.com"
-            },
-            {
-                id: 2,
-                username: "admin",
-                email: "admin@gmail.com"
-            }
-        ]
-        res.status(200).json(data);
+        try {
+            const data = await prisma.user.findMany();
+            res.status(200).json(data);
+        } catch (err) {
+            console.error("Database fetching data error: ", err);
+            res.status(500).json({ error: "Failed to fetch data"})
+        }
+    } else {
+        res.status(405).json({ error: "Method not allowed" });
     }
 }
-
-export default handler;
