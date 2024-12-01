071e8b29301b687e38c0dd08cbefdeea1d283e7d
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-12-01 10:14:32 +0530

    Update getallusers.ts

--- src/pages/api/getallusers.ts
+++ src/pages/api/getallusers.ts
@@ -1,22 +1,22 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "GET") {
-        // const data = await prisma.user.findMany();
-        const data = [
-            {
-                id: 1,
-                username: "GS",
-                email: "gurjotsingh.code@gmail.com"
-            },
-            {
-                id: 2,
-                username: "admin",
-                email: "admin@gmail.com"
-            }
-        ]
+        const data = await prisma.user.findMany();
+        // const data = [
+        //     {
+        //         id: 1,
+        //         username: "GS",
+        //         email: "gurjotsingh.code@gmail.com"
+        //     },
+        //     {
+        //         id: 2,
+        //         username: "admin",
+        //         email: "admin@gmail.com"
+        //     }
+        // ]
         res.status(200).json(data);
     }
 }
