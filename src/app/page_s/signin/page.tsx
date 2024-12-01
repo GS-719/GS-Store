f9c8b2e868353c3732f114fe1c9a663ec04d2945
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-12-01 17:22:48 +0530

    Create page.tsx

---
+++ src/app/page_s/signin/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React from 'react'
+import "@/styles/Signin.css";
+import { useState } from 'react';
+import axios from 'axios';
+
+const Page = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signin, setSignin] = useState('');
+  const SendReq = async () => {
+    const data = {
+        email: email,
+        password: password
+    }
+    const response = await axios.post("/api/signin", data, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const result = await response.data.message;
+    if (result === "0") {
+        setSignin("Login successfully....");
+    } else if (result === "1") {
+        setSignin("incorrect password");
+    } else if (result === "2") {
+        setSignin("User not found");
+    } else {
+        setSignin("Error while signing in: " + result);
+    }
+  }
+  return (
+    <div>
+      <div className='Main'>
+        <h1>Sign in</h1>
+        <p>{signin}</p>
+        <input className='SigninInput' onChange={e => setEmail(e.target.value)} type='email' placeholder='Email' />
+        <input className='SigninInput' onChange={e => setPassword(e.target.value)} type='password' placeholder='Password' />
+        <button onClick={SendReq} className='SigninButton'>Sign in</button>
+      </div>
+    </div>
+  )
+}
+
+export default Page
