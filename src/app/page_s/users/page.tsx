bd15894101bbca5092635304dce2e87f6c2df4a4
GS-719 <134775577+GS-719@users.noreply.github.com>
2024-12-02 19:56:12 +0530

    Update page.tsx

--- src/app/page_s/users/page.tsx
+++ src/app/page_s/users/page.tsx
@@ -1,50 +1,33 @@
 "use client"
 import React from 'react'
-import "@/styles/Signin.css";
-import { useState } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Page = () => {
-  const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [signin, setSignin] = useState('');
-  const SendReq = async () => {
-    const data = {
-        email: email,
-        password: password
+
+  const [Data, setData] = useState<Array<{ name: string; email: string }> | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("/api/getallusers");
+      const data = await response.data;
+      setData(data);
     }
-    const response = await axios.post("/api/signin", data, {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'signin',
-        },
-    });
-    const result = await response.data.message;
-    if (result === "0") {
-        setSignin("Login successfully....");
-        router.push('/page_s/account/dashboard');
-    } else if (result === "1") {
-        setSignin("incorrect password");
-    } else if (result === "2") {
-        setSignin("User not found");
-    } else {
-        setSignin("Error while signing in: " + result);
-    }
-  }
-  
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <div className='Main'>
-        <h1>Sign in</h1>
-        <p>{signin}</p>
-        <input className='SigninInput' onChange={e => setEmail(e.target.value)} type='email' placeholder='Email' />
-        <input className='SigninInput' onChange={e => setPassword(e.target.value)} type='password' placeholder='Password' />
-        <button onClick={SendReq} className='SigninButton'>Sign in</button>
-      </div>
+      <h1>All Users</h1>
+      {Data && (
+        <ul>
+          {Data.map((item, index) => (
+            <li key={index}>{item.name} - {item.email}</li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
 
-export default Page; 
+export default Page
